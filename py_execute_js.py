4825f13c463b3810163bf615c2f1e214783c7fa2
GeorgeRobots <1258501438@qq.com>
2018-08-02 11:19:33 +0800

    Add files via upload

--- py_execute_js.py
+++ py_execute_js.py
@@ -7,6 +7,8 @@ import execjs
 import random
 import time
 import writ
+import reading
+import datetime
 #import uuid
 '''another way to get guid'''
 #guid=uuid.uuid4()
@@ -18,75 +20,133 @@ def generate_guid():
     return createGuid() + createGuid() + "-" + createGuid() + "-" + createGuid() + createGuid() + "-" + createGuid() + createGuid() + createGuid(); 
 
 def get_data():
-    guid = generate_guid()
-    fh=open("D:\\FDU\\Template\\NLP(ZhipengXie)\\Spider\\getkey.js","r")
-    js_all = fh.read()
-    fh.close()
-    cjar=http.cookiejar.CookieJar()
-    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cjar))
-    opener.addheaders=[("Referer","http://wenshu.court.gov.cn/List/List?sorttype=1&conditions=searchWord+1++%E5%88%91%E4%BA%8B%E6%A1%88%E4%BB%B6+%E6%A1%88%E4%BB%B6%E7%B1%BB%E5%9E%8B:%E5%88%91%E4%BA%8B%E6%A1%88%E4%BB%B6")]
-    urllib.request.install_opener(opener)
-     
-    uapools=[
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.22 Safari/537.36 SE 2.X MetaSr 1.0",
-        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)",
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
-        ]
-    urllib.request.urlopen("http://wenshu.court.gov.cn/List/List?sorttype=1&conditions=searchWord+1++%E5%88%91%E4%BA%8B%E6%A1%88%E4%BB%B6+%E6%A1%88%E4%BB%B6%E7%B1%BB%E5%9E%8B:%E5%88%91%E4%BA%8B%E6%A1%88%E4%BB%B6").read().decode("utf-8","ignore")
-    pat="vjkl5=(.*?)\s"
-    vjkl5=re.compile(pat,re.S).findall(str(cjar))
-    if(len(vjkl5)>0):
-        vjkl5=vjkl5[0]
-    else:
-        vjkl5=0
-    compile_js=execjs.compile(js_all)
-    vl5x=compile_js.call("get_key",str(vjkl5))
-    url="http://wenshu.court.gov.cn/List/ListContent"
-    #100页,restricted by the website
-    for i in range(0,100):
-        try:
-            codeurl="http://wenshu.court.gov.cn/ValiCode/GetCode"
-            codedata=urllib.parse.urlencode({
-                                    "guid":guid,
-                                    }).encode('utf-8')
-            codereq = urllib.request.Request(codeurl,codedata)
-            codereq.add_header('User-Agent',random.choice(uapools))
-            codedata=urllib.request.urlopen(codereq).read().decode("utf-8","ignore")
-            postdata =urllib.parse.urlencode({
-                                    "Param":"案件类型:刑事案件",
-                                    "Index":str(i+1),
-                                    "Page":"20",
-                                    "Order":"法院层级",
-                                    "Direction":"asc",
-                                    "number":str(codedata),
-                                    "guid":guid,
-                                    "vl5x":vl5x,
-                                    }).encode('utf-8')        
-            req = urllib.request.Request(url,postdata)
-            req.add_header('User-Agent',random.choice(uapools))
-            data=urllib.request.urlopen(req).read().decode("utf-8","ignore")
-            pat1='文书ID.*?".*?"(.*?)."'
-            pat2='裁判日期.*?".*?"(.*?)."'
-            pat3='案件名称.*?".*?"(.*?)."'
-            pat4='审判程序.*?".*?"(.*?)."'
-            pat5='案号.*?".*?"(.*?)."'
-            pat6='法院名称.*?".*?"(.*?)."'
-            allid1=re.compile(pat1).findall(data)
-            tian=open('Doc_Id.txt','a')
-            for j in allid1:
-                tian.write(j)
-                tian.write('\n')
-            tian.close()
-        except Exception as err:
-            print(err)
+    type_of_case = "执行案件"
+    case_type_list = {"刑事案件":'1',"民事案件":'2',"行政案件":'3',"赔偿案件":'4',"执行案件":'5'}
+    trans_type_list = {"刑事案件":"%E5%88%91%E4%BA%8B%E6%A1%88%E4%BB%B6",
+    "民事案件":"%E6%B0%91%E4%BA%8B%E6%A1%88%E4%BB%B6",
+    "行政案件":"%E8%A1%8C%E6%94%BF%E6%A1%88%E4%BB%B6",
+    "赔偿案件":"%E8%B5%94%E5%81%BF%E6%A1%88%E4%BB%B6",
+    "执行案件":"%E6%89%A7%E8%A1%8C%E6%A1%88%E4%BB%B6"}
+    eng_name_list = {"刑事案件":"crimi_case",
+    "民事案件":"civil_case",
+    "行政案件":"admin_case",
+    "赔偿案件":"compe_case",
+    "执行案件":"execu_case"}
 
-        time.sleep(int((1+random.random())*20))
+    sta_date = '2015-01-01'
+    end_date = '2015-12-30'
+    date_sta = datetime.datetime.strptime(sta_date,'%Y-%m-%d')
+    date_end = datetime.datetime.strptime(end_date,'%Y-%m-%d')
+    provinces = \
+    ["最高人民法院","北京市","天津市","河北省","山西省",
+    "内蒙古自治区","辽宁省","吉林省","黑龙江省","上海市",
+    "江苏省","浙江省","安徽省","福建省","江西省",
+    "山东省","河南省","湖北省","湖南省","广东省",
+    "广西壮族自治区","海南省","重庆市","四川省","贵州省",
+    "云南省","西藏自治区","陕西省","甘肃省","青海省",
+    "宁夏回族自治区","新疆维吾尔自治区",
+    "新疆维吾尔自治区高级人民法院生产建设兵团分院"
+    ]
+    #to construct the first param of Form_Data's parameter
+    while date_sta < date_end:
+        date_sta += datetime.timedelta(days = 1)
+        data_head = date_sta.strftime('%Y-%m-%d')#from
+        date_tail = (date_sta+datetime.timedelta(days = 1)).strftime('%Y-%m-%d')#to
+
+        for prov in provinces:
+            referer_str = "http://wenshu.court.gov.cn/List/List?sorttype=1&conditions=searchWord+++"+\
+            data_head+"%20TO%20"+date_tail+"%E4%B8%8A%E4%BC%A0%E6%97%A5%E6%9C%9F:"+data_head+"%20TO%20"+date_tail+\
+            "&conditions=searchWord+2+AJLX++"+"%E6%A1%88%E4%BB%B6%E7%B1%BB%E5%9E%8B:"+trans_type_list[type_of_case]
+
+            guid = generate_guid()
+            fh=open("D:\\FDU\\Template\\NLP(ZhipengXie)\\Spider\\"+eng_name_list[type_of_case]+"\\getkey.js","r")
+            js_all = fh.read()
+            fh.close()
+            cjar=http.cookiejar.CookieJar()
+            opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cjar))
+            opener.addheaders=[("Referer",referer_str)]
+            urllib.request.install_opener(opener)
+            uapools=[
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
+                "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.22 Safari/537.36 SE 2.X MetaSr 1.0",
+                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)",
+                "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
+                ]
+            try:
+                # fir_package = urllib.request.urlopen(referer_str)
+                # res_num = fir_package.getcode()
+                # if res_num != 200:
+                #     print("Fir:%d"%(res_num))
+                #     continue
+                # fir_package.read().decode("utf-8","ignore")
+                urllib.request.urlopen(referer_str).read().decode("utf-8","ignore")
+                pat="vjkl5=(.*?)\s"
+                vjkl5=re.compile(pat,re.S).findall(str(cjar))
+                if(len(vjkl5)>0):
+                    vjkl5=vjkl5[0]
+                else:
+                    vjkl5=0
+                compile_js=execjs.compile(js_all)
+                vl5x=compile_js.call("get_key",str(vjkl5))
+                url="http://wenshu.court.gov.cn/List/ListContent"
+                #100页,restricted by the website
+                for i in range(0,10):
+                    try:
+                        codeurl="http://wenshu.court.gov.cn/ValiCode/GetCode"
+                        codedata=urllib.parse.urlencode({
+                                                "guid":guid,
+                                                }).encode('utf-8')
+                        codereq = urllib.request.Request(codeurl,codedata)
+                        codereq.add_header('User-Agent',random.choice(uapools))
+                        # sec_package = urllib.request.urlopen(codereq)
+                        # resu_num = sec_package.getcode()
+                        # if resu_num != 200:
+                        #     print("Sec:%d"%(resu_num))
+                        #     continue
+                        # codedata = sec_package.read().decode("utf-8","ignore")
+                        codedata=urllib.request.urlopen(codereq).read().decode("utf-8","ignore")
+                        postdata =urllib.parse.urlencode({
+                                                "Param":"上传日期:"+data_head+" TO "+date_tail+",案件类型:"+type_of_case+",法院地域:"+prov,
+                                                "Index":str(i+1),
+                                                "Page":"5",
+                                                "Order":"法院层级",
+                                                "Direction":"asc",
+                                                "number":str(codedata),
+                                                "guid":guid,
+                                                "vl5x":vl5x,
+                                                }).encode('utf-8')        
+                        req = urllib.request.Request(url,postdata)
+                        req.add_header('User-Agent',random.choice(uapools))
+                        # thi_package = urllib.request.urlopen(req)
+                        # resul_num = thi_package.getcode()
+                        # if resul_num != 200:
+                        #     print("Thi:%d"%(resul_num))
+                        #     continue
+                        # data = thi_package.read().decode("utf-8","ignore")
+                        data=urllib.request.urlopen(req).read().decode("utf-8","ignore")
+                        pat1='文书ID.*?".*?"(.*?)."'
+                        pat2='裁判日期.*?".*?"(.*?)."'
+                        pat3='案件名称.*?".*?"(.*?)."'
+                        pat4='审判程序.*?".*?"(.*?)."'
+                        pat5='案号.*?".*?"(.*?)."'
+                        pat6='法院名称.*?".*?"(.*?)."'
+                        allid1=re.compile(pat1).findall(data)
+                        tian=open("D:\\FDU\\Template\\NLP(ZhipengXie)\\Spider\\"+eng_name_list[type_of_case]+"\\Doc_Id.txt",'a')
+                        for j in allid1:
+                            tian.write(j)
+                            tian.write('\n')
+                        tian.close()
+                    except Exception as err:
+                        print(err)
+
+                    time.sleep(int((1+random.random())*5))
+            except Exception as err:
+                print(err)
 
 if __name__ == '__main__':
     get_data()
     counter = writ.get_text()
     for i in range(counter):
         file_path = "ws_"+str(i)+".txt"
-        writ.extract_text(file_path)
+        reading.extract_text(file_path)
--- reading.py
+++ reading.py
@@ -1,24 +1,82 @@
 import sys,os
 from bs4 import BeautifulSoup as bs
+import re
+CODE_TYPE = 'gb18030'
 
 def extract_text(file_path):
     #read data
-    fh = open(file_path,"r")
-    raw_data = fh.read()
-    if len(raw_data) < 50:
+    fh = open(file_path,"r",errors = 'ignore')#encoding = 'gb18030'
+    raw_datas = fh.read()
+    new_data = raw_datas#used to catch 'legalbase' text
+
+    if len(raw_datas) < 150:
         fh.close()
         return 
     fh.close()
+    #to find the accurate scope of text body
+    sta_pos = raw_datas.find('Html\\\":\\\"')
+    end_pos = raw_datas.rfind("</div>")
+    raw_data = raw_datas[sta_pos+9:end_pos+6]
     temp_str_list = file_path.split(".")
-    new_file_path = temp_str_list[0]+"_cooked.txt"
-    #write data
-    fw = open(new_file_path,"a+")
-    bs_obj = bs(raw_data,"html.parser")
-    #bs_obj.prettify()
-    list_div = bs_obj.find_all("div")
+    #new_file_paths = temp_str_list[0]+"_cooked.txt"
+    #new_file_path = new_file_paths.replace("ws","data")
+    new_file_path = "D:\\FDU\\Template\\NLP(ZhipengXie)\\Spider\\civil_case\\yuliaoku.txt"
+    # write data
+    fw = open(new_file_path,"a+",errors = 'ignore')
+    #bs_obj = bs(raw_data,"html.parser")
+    ##bs_obj.prettify()
+    #list_div = bs_obj.find_all("div")
+    
+    #used to get title and treat it as the key words extracted from the writ
+    title_writ = re.findall(r"attr\(\"title\"\,\"(.+?)\"\)\;\$\(\"\#tdSource",raw_datas)[0]
+    fw.write(title_writ+'\n')
+
+    #used to get the function part
+    # legal_raw = re.search(r"LegalBase:(.+?)length",new_data)
+    # leg_sta,leg_end = legal_raw.span()
+    # if leg_end-leg_sta > 20:
+    #     legal_fir = new_data[leg_sta:leg_end]
+    #     legal_spl = legal_fir.split("法规名称:")
+    #     for l1 in range(1,len(legal_spl)):
+    #         temp_rule_name = re.findall(r"(.+?),Items",legal_spl[l1])[0]
+    #         legal_aspl = legal_spl[l1].split("法条名称:")
+    #         for l2 in range(1,len(legal_aspl)):
+    #             temp_text_name = re.findall(r"(.+?),法条内容",legal_aspl[l2])[0]
+    #             if len(temp_text_name) == 2:
+    #                 fw.write(re.findall(r"法条内容:\'(.+?)\[ly\]",legal_aspl[l2])[0])
+    #             else:
+    #                 fw.write(re.sub('\'','',temp_rule_name+temp_text_name)+'\n')
+
+    #used to catch the reason part 
+    seg_type = ["WBSB","DSRXX","SSJL","AJJBQK","CPYZ","PJJG","WBWB"]
+    #对应于：首部，（unknown），事实，理由，案件基本情况，判决结果，尾部
+    seg_list = []
+    for seg_i in seg_type:
+        seg_temp = raw_data.find("<a type=\'dir\' name=\'"+seg_i+"\'>")
+        if seg_temp < 0:
+            if len(seg_list) == 0:
+                seg_temp = 0
+            else:
+                seg_temp = seg_list[-1]
+        seg_list.append(seg_temp)
+    raw_list = [raw_data[0:seg_list[0]]]
+    for raw_i in range(6):
+        raw_list.append(raw_data[seg_list[raw_i]:seg_list[raw_i+1]])
+    raw_list.append(raw_data[seg_list[-1]:])
+
+    for str_temp in raw_list:
+        bs_obj = bs(str_temp,"html.parser")
+        list_div = bs_obj.find_all("div")
+        temp_res = ""
+        for str_cook in list_div:
+            if str_cook.text != "":
+                temp_res += str_cook.text+'\n'
+        fw.write(temp_res)
     result_str = ""
     for str_temp in list_div:
         result_str += str_temp.text+'\n'
-    print(result_str)
-    fw.write(result_str)
     fw.close()
+
+if __name__ == "__main__":
+    #extract_text("D:\\FDU\\Template\\NLP(ZhipengXie)\\Spider\\civil_case\\data\\ws_9.txt")
+    extract_text("D:\\FDU\\Template\\NLP(ZhipengXie)\\Spider\\f.txt")
